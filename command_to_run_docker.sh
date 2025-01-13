f533b02feb3779fe45b5db4438f353b8110776f4
Baldanmatt <baldan.matteo98@gmail.com>
2025-01-13 14:23:14 +0100

    Change name of main into load_data.py in orer to avoid conflicts of merging with future train dev branch

--- command_to_run_docker.sh
+++ command_to_run_docker.sh
@@ -1,2 +1,2 @@
-docker run --rm -it -v "$(pwd)/data:/app/data" -v "$(pwd)/src:/app/src" sbexam:latest python3 src/main.py
+docker run --rm -it -v "$(pwd)/data:/app/data" -v "$(pwd)/src:/app/src" sbexam:latest python3 src/load_data.py
 
