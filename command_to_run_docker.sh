ed3d2efca4d1c0f335e7e2c41b98b4bf49e565da
Baldanmatt <baldan.matteo98@gmail.com>
2025-01-13 14:02:50 +0100

    Add docker version of the actual dependencies

---
+++ command_to_run_docker.sh
@@ -0,0 +1,2 @@
+docker run --rm -it -v "$(pwd)/data:/app/data" -v "$(pwd)/src:/src" sbexam:latest /bin/bash
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from pathlib import Path
-
+import gzip
+import pandas as pd
+import polars as pl
+DHS_INDEX_FILE = "DHS_Index_and_Vocabulary_hg38_WM20190703"
+FDR01_hg38_FILE = "dat_bin_FDR01_hg38"
+TXT_SUFFIX = "txt"
+CSV_SUFFIX = "csv"
 def main(data_dir):
     print("Starting main function...")
     print(f"\tThe data directory is located at: {data_dir}")
+    filename = FDR01_hg38_FILE
+    suffix = TXT_SUFFIX
+    with gzip.open(data_dir / f"{filename}.{suffix}.gz", "r") as f:
+        file_content=f.readlines()
+        # split each line into columns, tab separated
+        file_content=[line.decode("utf-8").split("\t") for line in file_content]
+        # Transform in a pandas dataframe
+        file_content=pl.DataFrame(file_content)
+        print("The file content is:\n", file_content)
+
     pass
 
 
